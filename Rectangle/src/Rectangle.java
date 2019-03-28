f54c6757260f22caa7ed33a9b90e0b6372c36552
KasiraDh <dhanunjaya.kasiraju@adp.com>
2019-03-28 21:00:20 +0530

    Reactangle class

---
+++ Rectangle/src/Rectangle.java
@@ -0,0 +1,11 @@
+
+public class Rectangle {
+	private double length;
+	private double width;
+	private double perimeter;
+	public Rectangle(double length,double width) {
+		this.length=length;
+		this.width=width;
+		this.perimeter=2*(this.length+this.width);
+	}
+}
