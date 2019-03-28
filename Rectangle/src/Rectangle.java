1af4150bb0bda99b3f2ae04db09d48fa088744f1
KasiraDh <dhanunjaya.kasiraju@adp.com>
2019-03-28 21:08:54 +0530

    added area

--- Rectangle/src/Rectangle.java
+++ Rectangle/src/Rectangle.java
@@ -3,12 +3,21 @@ public class Rectangle {
 	private double length;
 	private double width;
 	private double perimeter;
-	public Rectangle(double length,double width) {
-		this.length=length;
-		this.width=width;
-		this.perimeter=2*(this.length+this.width);
+	private double area;
+	private static final int NUMBER_OF_EQUAL_SIDES = 2;
+
+	public Rectangle(double length, double width) {
+		this.length = length;
+		this.width = width;
+		this.perimeter = NUMBER_OF_EQUAL_SIDES * (this.length + this.width);
+		this.area = this.length * this.width;
 	}
+
 	public double getPerimeter() {
 		return perimeter;
+	}
+
+	public double getArea() {
+		return area;
 	}
 }
